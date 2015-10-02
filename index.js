1f5d22cd60b5d73db573c3c3dd41566ff3258244
Murthy10 <samuel.kurath@gmail.com>
2015-10-03 01:41:25 +0200

    Hello World!

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello')
+/**
+ * A very simple node web server that will respond to requests
+ * with the Tropo WebAPI JSON version of "Hello, World!"
+ */
+
+var http = require('http');
+var tropowebapi = require('tropo-webapi');
+
+var server = http.createServer(function (request, response) {
+
+    // Create a new instance of the TropoWebAPI object.
+    var tropo = new tropowebapi.TropoWebAPI();
+    tropo.say("Hello, World!");
+
+    // Render out the JSON for Tropo to consume.
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(tropowebapi.TropoJSON(tropo));
+
+}).listen(80); // Listen on port 80 for requests.
---
+++ tropo/tropo.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end('Hello Http');
+});
+server.listen(8080);
+
+
+var tropo = require('tropo');
