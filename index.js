8df94f52053f6852fa3616a5a51a21d78495c76f
Lukas Martinelli <me@lukasmartinelli.ch>
2015-10-03 23:38:32 +0200

    Refactor and try add support languages

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser')
 var sbb = require('./sbb/connection.js');
 var io = require('socket.io');
 var serveStatic = require('serve-static');
-var stations = require('./stations.json');
+var stationData = require('./stations.json');
 
 var port = process.env.VCAP_APP_PORT || 3000;
 
@@ -18,21 +18,56 @@ io = io.listen(app.listen(port));
 // Stateful session store
 var sessions = {};
 
-
-function filterCallableStations(choix) {
-    return choix.filter(function(val) {
-        return val.indexOf("-") == -1 && val.indexOf("(") == -1 && val.indexOf(".") == -1 && val.indexOf("'") == -1 && val.indexOf("/") == -1 && val.indexOf("é") == -1;
+// Some stations cannot be parsed into a grammar so they need to be filtered out
+function allowStationsByGrammar(choices) {
+    return choices.filter(function(val) {
+        return val.indexOf("-") == -1
+            && val.indexOf("(") == -1
+            && val.indexOf(".") == -1
+            && val.indexOf("'") == -1
+            && val.indexOf("/") == -1
+            && val.indexOf("é") == -1;
     });
-};
+}
 
-var sessions = {};
+function createStationsGrammar() {
+    var goodStations = allowStationsByGrammar(stations.stations);
+    return stationData.stations(goodStations.join(", ")
+}
+
+function askLanguage(tropo) {
+    tropo.say("Guten Tag, herzlich willkommen bei ihrer SBB Auskunft!", null, null, null, null, "Stefan");
+
+    tropo.say("Für Deutsch drücken Sie die 1.", null, null, null, null, "Stefan");
+    tropo.say("Pour français touche 2", null, null, null, null, "Florence");
+    
+    var choices = new Choices("1,2,3");
+    var say = new Say("For english press 3");
+    tropo.ask(choices, null, null, null, "digit", null, null, say, 60, "Veronica");
+    return tropo;
+}
 
 app.post('/', function(req, res){
+    var tropo = askLanguage(new TropoWebAPI());
+    tropo.on("continue", null, "/where", true);
+
+    var callId = req.body.session.from.id;
+    var sessionId = req.body.session.id;
+
+    sessions[sessionId] = {
+        callId: callId
+    };
+
+    res.send(TropoJSON(tropo));
+});
+
+app.post('/where', function(req, res){
     var tropo = new TropoWebAPI();
+    console.log(req.body);
 
     var say = new Say("Willkommen bei SBB! Wohin möchten Sie fahren?", null, null, null, null, "Stefan");
 
-    var choices = new Choices(filterCallableStations(stations.stations).join(", "));
+    var choices = new Choices(createStationsGrammar());
     var recognizer = "de-de";
     tropo.ask(choices, 3, null, null, "destination", recognizer, null, say, null, "Stefan");
     tropo.on("continue", null, "/destination", true);
@@ -64,8 +99,8 @@ app.post('/destination', function(req, res){
         var choix = stations.stations.filter(function (val) {
             return val.indexOf(" ") == -1 && val.indexOf("-") == -1 && val.indexOf("(") == -1 && val.indexOf(".") == -1;
         });
-        var choices = new Choices(filterCallableStations(stations.stations).join(", "));
 
+        var choices = new Choices(createStationsGrammar());
         var recognizer = "de-de";
         tropo.ask(choices, 3, null, null, "departure", recognizer, null, say, null, "Stefan");
         tropo.on("continue", null, "/departure", true);
@@ -96,6 +131,10 @@ app.post('/departure', function(req, res){
             } else {
                 var firstStation = "" + response[0];
                 tropo.say(firstStation, null, null, null, null, "Stefan");
+
+                tropo.call(session.callId, null, null, null, null, null, "SMS", null, null, null);
+                tropo.say(response.toString());
+
                 // Twilio Credentials
                 var accountSid = 'AC8e449a90cfd0453b35f680291649ad18';
                 var authToken = 'cdec6c3325b55ba12e9a9973c89d828d';
