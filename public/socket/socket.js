8a34f2388435a808744a64ea840a963019463644
Murthy10 <samuel.kurath@gmail.com>
2015-10-03 14:06:44 +0200

    style

--- public/socket/socket.js
+++ public/socket/socket.js
@@ -1,7 +1,8 @@
 var socket = io.connect();
+var destination;
 socket.on('destination', function(msg){
-    $("ol").append($('<li>').text(msg));
+    destination = msg;
 });
 socket.on('departure', function(msg){
-    $("ol").append($('<li>').text(msg));
+    var string = '<th>*</th><th>' + msg.value + '</th><th>' + destination.value + '</th>'
 });
