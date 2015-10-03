1f20a5710a86b04df35f1b76f3e6fd8a15d4e225
Manuel Roth <manuelroth@hotmail.ch>
2015-10-03 03:32:33 +0200

    Add more cities

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.post('/', function(req, res){
     var tropo = new TropoWebAPI();
 
     var say = new Say("Welcome to SBB! Where do you want to go?");
-    var choices = new Choices("Bern, Zurich");
+    var choices = new Choices("Aarau, Aarburg, Aarburg, Adliswil, Agno, Altdorf, Altstetten, Amriswil, Appenzell, Arbon, Ascona, Aubonne, Avenches, Baar, Baden, Basel, Bellinzona, Binningen, Bremgarten, Brugg, Buchs, Bulle, Bulach, Burgdorf, Cham, Chur, Coppet, Davos, Dietikon, Dubendorf, Ebikon, Eglisau, Einsiedlen, Elgg, Emmen, Erlach, Frauenfeld, Fribourg, Glarus, Gordola, Gossau, Herisau, Greifensee, Grenchen, Horgen, Horw, Huttwil, Ilanz, Ittigen, Klingnau, Kloten, Koniz, Kreuzlingen, Kriens, Kusnacht, Lenzburg, Lichtensteig, Liestal, Locarno, Lugano, Lucerne, Lyss, Maienfeld, Martigny, Meilen, Morges, Neuchatel, Nyon, Olten, Opfikon, Orbe, Payerne, Pratteln, Rapperswil, Rheinfelden, Richterswil, Romont, Rorschach, Ruti, St.Gallen, Sargans, Sarnen, Schaffhausen, Schlieren, Schwyz, Sempach, Sierre, Sion, Solothurn, Spiez, Spreitenbach, Splügen, Stafa, Sursee, Thalwil, Thun, Thusis, Untersee, Uznach, Uster, Uzwil, Vernier, Vevey, Visp, Volketswil, Wadenswil, Waldenburg, Walenstadt, Wallisellen, Wettingen, Wetzikon, Wil, Willisau, Winterthur, Wohlen, Zofingen, Zug, Zurzach, Zurich, Geneva, Basel, Lausanne, Bern, Winterthur, Lucerne, St.Gallen, Lugano, Biel, Thun, Koniz, Schaffhausen, Fribourg, Vernier, Chur, Neuchatel, Uster, Sion");
 
     tropo.ask(choices, 3, null, null, "destination", null, null, say, null, null);
     tropo.on("continue", null, "/destination", true);
@@ -33,7 +33,7 @@ app.post('/destination', function(req, res){
     tropo.say("Your destination is " + destination);
 
     var say = new Say("From where do you want to start?");
-    var choices = new Choices("Bern, Zurich");
+    var choices = new Choices("Aarau, Aarburg, Aarburg, Adliswil, Agno, Altdorf, Altstetten, Amriswil, Appenzell, Arbon, Ascona, Aubonne, Avenches, Baar, Baden, Basel, Bellinzona, Binningen, Bremgarten, Brugg, Buchs, Bulle, Bulach, Burgdorf, Cham, Chur, Coppet, Davos, Dietikon, Dubendorf, Ebikon, Eglisau, Einsiedlen, Elgg, Emmen, Erlach, Frauenfeld, Fribourg, Glarus, Gordola, Gossau, Herisau, Greifensee, Grenchen, Horgen, Horw, Huttwil, Ilanz, Ittigen, Klingnau, Kloten, Koniz, Kreuzlingen, Kriens, Kusnacht, Lenzburg, Lichtensteig, Liestal, Locarno, Lugano, Lucerne, Lyss, Maienfeld, Martigny, Meilen, Morges, Neuchatel, Nyon, Olten, Opfikon, Orbe, Payerne, Pratteln, Rapperswil, Rheinfelden, Richterswil, Romont, Rorschach, Ruti, St.Gallen, Sargans, Sarnen, Schaffhausen, Schlieren, Schwyz, Sempach, Sierre, Sion, Solothurn, Spiez, Spreitenbach, Splügen, Stafa, Sursee, Thalwil, Thun, Thusis, Untersee, Uznach, Uster, Uzwil, Vernier, Vevey, Visp, Volketswil, Wadenswil, Waldenburg, Walenstadt, Wallisellen, Wettingen, Wetzikon, Wil, Willisau, Winterthur, Wohlen, Zofingen, Zug, Zurzach, Zurich, Geneva, Basel, Lausanne, Bern, Winterthur, Lucerne, St.Gallen, Lugano, Biel, Thun, Koniz, Schaffhausen, Fribourg, Vernier, Chur, Neuchatel, Uster, Sion");
     tropo.ask(choices, 3, null, null, "departure", null, null, say, null, null);
     tropo.on("continue", null, "/departure", true);
 
