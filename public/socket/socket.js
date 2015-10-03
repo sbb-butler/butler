6831a411d7dec471892e6095363171d474ba0de6
Murthy10 <samuel.kurath@gmail.com>
2015-10-03 14:22:47 +0200

    append row

--- public/socket/socket.js
+++ public/socket/socket.js
@@ -1,8 +1,10 @@
 var socket = io.connect();
 var destination;
 socket.on('destination', function(msg){
-    destination = msg;
+    destination = msg.toString();
 });
+
 socket.on('departure', function(msg){
-    var string = '<th>*</th><th>' + msg.value + '</th><th>' + destination.value + '</th>'
+    var row = '<tr><th>*</th><th>' + msg.toString() + '</th><th>' + destination.value + '</th></tr>';
+    $('tbody').append(row);
 });
