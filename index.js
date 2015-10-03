439111170f4a064e7b04596801dd2a035d4cc2fd
Lukas Martinelli <me@lukasmartinelli.ch>
2015-10-04 00:07:24 +0200

    Support multiple languages asking destination

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ var stationData = require('./stations.json');
 
 var port = process.env.VCAP_APP_PORT || 3000;
 
+var languages = {
+    german: {
+        voice: "Stefan",
+        recognizer: "de-de",
+        whereToGo: "Wohin wollen Sie reisen?"
+    },
+    french: {
+        voice: "Florence",
+        recognizer: "fr-fr",
+        whereToGo: "Quelle destination?"
+    },
+    english: {
+        voice: "Veronica",
+        recognizer: "en-us",
+        whereToGo: "Where do you want to go?"
+    }
+}
+
 app.use(bodyParser.json());
 app.use(serveStatic(__dirname + '/public'));
 io = io.listen(app.listen(port));
@@ -30,26 +48,26 @@ function allowStationsByGrammar(choices) {
     });
 }
 
-function createStationsGrammar() {
+function createStationsGrammar(language) {
     var goodStations = allowStationsByGrammar(stations.stations);
     return stationData.stations(goodStations.join(", "));
 }
 
 function askLanguage(tropo) {
-    tropo.say("Guten Tag, herzlich willkommen bei ihrer SBB Auskunft!", null, null, null, null, "Stefan");
+    tropo.say("Guten Tag, herzlich willkommen bei ihrer SBB Auskunft!", null, null, null, null, languages.german.voice);
 
-    tropo.say("Für Deutsch drücken Sie die 1.", null, null, null, null, "Stefan");
-    tropo.say("Pour français touche 2", null, null, null, null, "Florence");
+    tropo.say("Für Deutsch drücken Sie die Eins.", null, null, null, null, languages.german.voice);
+    tropo.say("Pour français touche deux", null, null, null, null, languages.french.voice);
     
     var choices = new Choices("1,2,3");
-    var say = new Say("For english press 3");
-    tropo.ask(choices, null, null, null, "digit", null, null, say, 60, "Veronica");
+    var say = new Say("For english press three");
+    tropo.ask(choices, null, null, null, "digit", null, null, say, 60, languages.english.voice);
     return tropo;
 }
 
 app.post('/', function(req, res){
     var tropo = askLanguage(new TropoWebAPI());
-    tropo.on("continue", null, "/where", true);
+    tropo.on("continue", null, "/askDestination", true);
 
     var callId = req.body.session.from.id;
     var sessionId = req.body.session.id;
@@ -61,28 +79,37 @@ app.post('/', function(req, res){
     res.send(TropoJSON(tropo));
 });
 
-app.post('/where', function(req, res){
+function languageFromNumber(chosenNumber) {
+    var language = languages.german;
+    if(chosenNumber === "1") {
+        language = languages.german;
+    } else if (chosenNumber === "2") {
+        language = languages.french;
+    } else if (chosenNumber === "3") {
+        language = languages.english;
+    }
+    return language;
+}
+
+app.post('/askDestination', function(req, res){
     var tropo = new TropoWebAPI();
     console.log(req.body);
+    
+    if(req.body.result.actions) {
+        var chosenNumber = req.body.result.actions.value;
+        var sessionId = req.body.result.sessionId;
+        var language = languageFromNumber(chosenNumber);
 
-    var say = new Say("Willkommen bei SBB! Wohin möchten Sie fahren?", null, null, null, null, "Stefan");
+        sessions[sessionId].language = language;
+        var say = new Say(language.whereToGo, null, null, null, null, language.voice);
+        var choices = new Choices(createStationsGrammar(language));
+        tropo.ask(choices, 3, null, null, "destination", language.recognizer, null, say, null, language.voice);
 
-    var choices = new Choices(createStationsGrammar());
-    var recognizer = "de-de";
-    tropo.ask(choices, 3, null, null, "destination", recognizer, null, say, null, "Stefan");
-    tropo.on("continue", null, "/destination", true);
-    tropo.on("incomplete", null, "/incomplete", true);
+        tropo.on("continue", null, "/destination", true);
+        tropo.on("incomplete", null, "/incomplete", true);
 
-
-
-    var callId = req.body.session.from.id;
-    var sessionId = req.body['session']['id'];
-    sessions[sessionId] = {
-        callId: callId
-    };
-
-
-    res.send(TropoJSON(tropo));
+        res.send(TropoJSON(tropo));
+    }
 });
 
 app.post('/destination', function(req, res){
