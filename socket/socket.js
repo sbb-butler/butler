85c101d5a76d866869b19a0de9cdd2c7900361ac
Murthy10 <samuel.kurath@gmail.com>
2015-10-03 10:57:16 +0200

    append list

--- socket/socket.js
+++ socket/socket.js
@@ -1,7 +1,8 @@
 var socket = io.connect('http://localhost', { port: 80 });
 socket.on('destination', function(msg){
     console.log(msg);
+    $("ol").append($('<li>').text(msg));
 });
 socket.on('departure', function(msg){
-    console.log(msg);
+    $("ol").append($('<li>').text(msg));
 });
