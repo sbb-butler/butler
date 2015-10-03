f948062a9ae3a79ba6387ab462f564ccc0849c60
Murthy10 <samuel.kurath@gmail.com>
2015-10-03 18:20:08 +0200

    fixed error handling

--- sbb/connection.js
+++ sbb/connection.js
@@ -5,26 +5,27 @@ module.exports = function(from, to, callback) {
 	request('http://transport.opendata.ch/v1/connections?limit=1&from='+from+'&to='+to, function(error, res, body) {
 		if(error){
 			callback(error, [])
+		}else {
+			var connection = JSON.parse(body);
+			var sections = connection.connections[0].sections;
+			var stations = [];
+			var time = new Date(connection.connections[0].from.departure);
+			moment().utcOffset(time);
+			var hour = moment(time).subtract(1, 'days').format('hh');
+			var minutes = moment(time).subtract(1, 'days').format('mm');
+			var departure = hour + " Uhr " + minutes;
+			stations.push("Zug fährt von " + from + " auf Gleis " + connection.connections[0].from.platform + " um " + departure + " ab");
+			for (var i in sections) {
+				//console.log(sections[i]);
+				var arrival = sections[i].arrival;
+				var arrivalTime = new Date(arrival.arrival);
+				moment().utcOffset(arrivalTime);
+				var hour = moment(arrivalTime).subtract(1, 'days').format('hh');
+				var minutes = moment(arrivalTime).subtract(1, 'days').format('mm');
+				var departureTime = hour + " Uhr " + minutes;
+				stations.push("Ankunft in " + arrival.station.name + " auf Gleis " + arrival.platform + " um " + departureTime);
+			}
+			callback(error, stations);
 		}
-		var connection = JSON.parse(body);
-		var sections = connection.connections[0].sections;
-		var stations = [];
-		var time = new Date(connection.connections[0].from.departure);
-		moment().utcOffset(time);
-		var hour = moment(time).subtract(1,'days').format('hh');
-		var minutes = moment(time).subtract(1,'days').format('mm');
-        var departure = hour + " Uhr " + minutes;
-		stations.push("Zug fährt von " + from + " auf Gleis " + connection.connections[0].from.platform + " um " + departure + " ab");
-		for(var i in sections) {
-			//console.log(sections[i]);
-			var arrival = sections[i].arrival;
-			var arrivalTime = new Date(arrival.arrival);
-			moment().utcOffset(arrivalTime);
-			var hour = moment(arrivalTime).subtract(1,'days').format('hh');
-			var minutes = moment(arrivalTime).subtract(1,'days').format('mm');
-	        var departureTime = hour + " Uhr " + minutes;
-			stations.push("Ankunft in "+arrival.station.name+" auf Gleis "+arrival.platform+" um "+departureTime);
-		}
-		callback(stations);
 	});
 }
