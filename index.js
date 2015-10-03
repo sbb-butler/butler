8e83023515ad6988332e005490ad5eee86a1e6d5
Lukas Martinelli <me@lukasmartinelli.ch>
2015-10-03 02:31:23 +0200

    Try to adapt code for SBB

--- index.js
+++ index.js
@@ -5,41 +5,29 @@ var tropo_webapi = require('tropo-webapi');
 var server = http.createServer(app)
 var bodyParser = require('body-parser')
 
-// Required to process the HTTP body.
-// req.body has the Object while req.rawBody has the JSON string.
-
 app.use(bodyParser.json());
 
 app.post('/', function(req, res){
-
     var tropo = new TropoWebAPI();
 
-    var say = new Say("What's your favorite color?  Choose from red, blue or green.");
-    var choices = new Choices("red, blue, green");
+    var say = new Say("Welcome to SBB! Where do you want to go?");
+    var choices = new Choices("Bern, Zurich");
 
-    // (choices, attempts, bargein, minConfidence, name, recognizer, required, say, timeout, voice);
-
-    tropo.ask(choices, 3, null, null, "color", null, null, say, null, null);
-
+    tropo.ask(choices, 3, null, null, "destination", null, null, say, null, null);
     tropo.on("continue", null, "/continue", true);
 
     res.send(TropoJSON(tropo));
-
 });
 
 app.post('/continue', function(req, res){
-
     var tropo = new TropoWebAPI();
 
-    var answer = req.body['result']['actions']['value'];
+    var destination = req.body['result']['actions']['value'];
+    tropo.say("Your destination is " + destination.value);
 
-    tropo.say("You said " + answer);
-
-    console.log(answer)
+    console.log(destination)
     res.send(TropoJSON(tropo));
-
 });
 
 app.listen(80);
 console.log('Server running on port :80');
-
